ad43537da045b13287e4efe1d37fc149b32bde64
Jeff Prestes <jeffprestes@gmail.com>
2020-01-27 14:32:11 -0300

    adjust links

--- test-service-worker.js
+++ test-service-worker.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('/qrcodepateo/service-worker.js', { scope: '/qrcodepateo/' })
       .then(function () {
-        console.log("Service Worker Registered, Cheers to PWA Fire!");
+        console.log("Service Worker Registered");
       });
   });
 }
